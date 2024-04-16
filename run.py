be58ae4825ef53c239e0d43f5e4a9bd7d5dc275c
Bartosz Nowak <bartekn337@gmail.com>
2024-04-16 08:39:26 +0200

    import issue fix

--- run.py
+++ run.py
@@ -3,17 +3,17 @@ from install import log_and_run
 if __name__ == "__main__":
     log_and_run([
         "cairo-run \
-        --program=simple_bootloader.json \
+        --program=bootloader.json \
         --layout=recursive_with_poseidon \
-        --program_input=simple_bootloader_input.json \
-        --air_public_input=simple_bootloader_public_input.json \
-        --air_private_input=simple_bootloader_private_input.json \
-        --trace_file=simple_bootloader_trace.json \
-        --memory_file=simple_bootloader_memory.json \
+        --program_input=bootloader_input.json \
+        --air_public_input=bootloader_public_input.json \
+        --air_private_input=bootloader_private_input.json \
+        --trace_file=bootloader_trace.json \
+        --memory_file=bootloader_memory.json \
         --print_output \
         --proof_mode \
         --print_info"
-    ], "Running cairo1 pie in cairo0 bootloader", cwd="cairo-lang")
+    ], "Running cairo1 pie in cairo0 bootloader", cwd=".")
 
 # cargo run ../../cairo1.cairo --layout recursive_large_output --trace_file cairo1/program.trace --memory_file cairo1/program.memory --air_public_input cairo1/air_public_input.json --air_private_input cairo1/air_private_input.json --proof_mode
 # cargo run ../../cairo1.cairo --layout recursive_large_output --print_output
